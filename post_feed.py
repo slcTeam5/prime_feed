282405de34b9771daf67046d9e0ac4a8fcc5e319
shyaka <shyaka>
2017-08-18 03:17:05 -0700

    Changed class name to meet PEP conventions

--- post_feed.py
+++ post_feed.py
@@ -2,11 +2,11 @@ import requests
 
 API_ENDPOINT = "http://34.207.10.230:3000/posts"
 
-class Make_post:
+
+class MakePost:
     def __init__(self, title, body, user_id, id):
         data = {"userId": user_id,
                 "id": id,
                 'title': title,
                 'body': body}
         r = requests.post(url=API_ENDPOINT, data=data)
-        
