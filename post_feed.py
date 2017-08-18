d1f6703fc12fb6582513727f5908795e237576a1
shyaka <shyaka>
2017-08-18 03:34:20 -0700

    Edited post function

--- post_feed.py
+++ post_feed.py
@@ -1,12 +1,12 @@
 import requests
 
-API_ENDPOINT = "http://34.207.10.230:3000/posts"
+API_ENDPOINT = "http://34.207.10.230:3000/posts/"
 
 
 class MakePost:
     def __init__(self, title, body, user_id, id):
         data = {"userId": user_id,
                 "id": id,
-                'title': title,
-                'body': body}
-        r = requests.post(url=API_ENDPOINT, data=data)
+                "title": title,
+                "body": body}
+        r = requests.post(url=API_ENDPOINT, json=data)
