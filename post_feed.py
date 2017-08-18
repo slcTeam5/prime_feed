5e545791ef26d37defd59f0fc2cbbd028adcc6db
shyaka <shyaka>
2017-08-18 02:54:32 -0700

    Made the function Object Oriented

--- post_feed.py
+++ post_feed.py
@@ -2,13 +2,11 @@ import requests
 
 API_ENDPOINT = "http://34.207.10.230:3000/posts"
 
-def post_feed(title, body, user_id, id):
-    # data to be sent to api
-    data = {"userId": user_id,
-            "id": id,
-            'title': title,
-            'body': body}
-    return data
-
-data = post_feed('Three People win awrds', 'hsdkfshkdfhasdf', 1, 1)
-r = requests.post(url = API_ENDPOINT, data = data)
+class Make_post:
+    def __init__(self, title, body, user_id, id):
+        data = {"userId": user_id,
+                "id": id,
+                'title': title,
+                'body': body}
+        r = requests.post(url=API_ENDPOINT, data=data)
+        
