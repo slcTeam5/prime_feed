eb1f700070fa728004b541bc04c5310c061f7598
shyaka <shyaka>
2017-08-18 03:37:52 -0700

    Changed order of fields

--- post_feed.py
+++ post_feed.py
@@ -5,8 +5,8 @@ API_ENDPOINT = "http://34.207.10.230:3000/posts/"
 
 class MakePost:
     def __init__(self, title, body, author, id):
-        data = {"author": author,
-                "id": id,
-                "title": title,
-                "body": body}
+        data = {"title": title,
+                "body": body,
+                "author": author,
+                "id": id}
         r = requests.post(url=API_ENDPOINT, json=data)
